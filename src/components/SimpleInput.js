f15f382807216935c9242c57e363641ad824ff62
Sagar Mochi <chouhansagar131@gmail.com>
2022-03-09 17:31:36 +0530

    refactor simpleInput.js removed one useState

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,42 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  
+
+  const [enteredName, setEnteredName] = useState("");
+  const [hasStartedEnteringName, setHasStartedEnteringName] = useState(false)
+
+  const isEnteredNameValid = enteredName.trim().length !== 0
+
+
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const handleOnFocusOut = (e) => {
+      setHasStartedEnteringName(true)
+  }
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    setHasStartedEnteringName(true)
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+          onBlur={handleOnFocusOut}
+        />
+        {!isEnteredNameValid && hasStartedEnteringName && <p className="error-text">Name must not be emtpy...</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button >Submit</button>
       </div>
     </form>
   );
