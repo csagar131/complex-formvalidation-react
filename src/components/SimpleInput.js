f6b6b4940c7be50498d0c9a5be0adbfbe8f2d231
Sagar Mochi <chouhansagar131@gmail.com>
2022-03-10 18:28:51 +0530

    complete form validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -10,20 +10,22 @@ const SimpleInput = () => {
     isTouched : isNameInputTouched
   } = useInputValidate((value) => value.trim().length !== 0);
 
-  //   const checkValid = (enteredValue) => {
-  //     if(enteredValue.name.trim().length !== 0 && String(enteredInput.email)
-  //     .toLowerCase()
-  //     .match(
-  //       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  //     )){
-  //       return true
-  //     }
-  //     return false
-  //   }
+  const {
+    value: enteredEmail,
+    isEnteredValueValid: isEnteredEmailValid,
+    inputChangeHandler : emailInputChangeHandler,
+    handleOnBlurInput : handleOnBlurEmail,
+    setIsTouched : setIsEmailInputTouched,
+    isTouched : isEmailInputTouched
+  } = useInputValidate((value) => String(value)
+  .toLowerCase()
+  .match(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  ));
 
   let isFormValid = false;
 
-  if (isEnteredNameValid) {
+  if (isEnteredNameValid && isEnteredEmailValid) {
     isFormValid = true;
   }
 
@@ -31,9 +33,8 @@ const SimpleInput = () => {
   const formSubmitHandler = (e) => {
     e.preventDefault();
     setIsNameInputTouched(true)
+    setIsEmailInputTouched(true)
   };
-
-  console.log()
 
   return (
     <form onSubmit={formSubmitHandler}>
@@ -51,20 +52,20 @@ const SimpleInput = () => {
           <p className="error-text">Name must not be emtpy...</p>
         )}
       </div>
-      {/* <div className="form-control">
+      <div className="form-control">
         <label htmlFor="name">Your Email</label>
         <input
           type="email"
           id="email"
           name="email"
-          value={enteredInput.email}
-          onChange={inputChangeHandler}
-          onBlur={handleOnFocusOutEmail}
+          value={enteredEmail}
+          onChange={emailInputChangeHandler}
+          onBlur={handleOnBlurEmail}
         />
         {!isEnteredEmailValid && isEmailInputTouched &&
           <p className="error-text">Pleas enter valid email...</p>
         }
-      </div> */}
+      </div>
       <div className="form-actions">
         <button disabled={!isFormValid}>Submit</button>
       </div>
